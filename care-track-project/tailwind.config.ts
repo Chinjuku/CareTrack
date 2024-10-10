49f9535977ecf6a8c393f6be41f9e86618d0dcee
MaledKhaoSan <sweetieton4321@gmail.com>
2024-10-10 13:52:39 +0700

    Add Frontend: User Side

--- care-track-project/tailwind.config.ts
+++ care-track-project/tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['IBM Plex Sans Thai', 'sans-serif'],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
