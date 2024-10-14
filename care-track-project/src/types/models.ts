7230ca27909420fd014732a061ccd384917901b2
Mean <anuwattipong123@gmail.com>
2024-10-14 17:23:24 +0700

    Refactor Dockerfile and .gitignore
    - Remove unnecessary lines from .gitignore
    - Refactor Dockerfile to use multi-stage build for better performance and smaller image size

--- care-track-project/src/types/models.ts
+++ care-track-project/src/types/models.ts
@@ -1,100 +1,97 @@
 export type LoginUser = {
-    id        : string
-    username  : string
-    email     : string
-    password  : string
-    role      : string
+  id        : string
+  username  : string
+  email     : string
+  password  : string
+  role      : string
 }
 
 export enum UserRoles {
-    PATIENT = "PATIENT",
-    STAFF = "STAFF",
+  PATIENT = "PATIENT",
+  STAFF = "STAFF",
 }
 
 export type RegisterUser = {
-    username  : string
-    email     : string
-    password  : string
-    role      : UserRoles
-    firstname : string
-    lastname  : string
+  username  : string
+  email     : string
+  password  : string
+  role      : UserRoles
+  firstname : string
+  lastname  : string
 }
 
 export type User = {
-    id: string;
-    email: string;
-    username: string;
-    role: string
-    patient?: Patient;
-    staff?: Staff;
+  id: string;
+  email: string;
+  username: string;
+  role: string
+  patient?: Patient;
+  staff?: Staff;
 }
 
 export type Patient = {
-    id: number;
-    firstname?: string | null;
-    lastname?: string | null;
-    medicationAllocations: MedicationAllocation[];
-    appointments: Appointment[];
-    userId: number;
-    user: User;
-  };
-  
-  export type Staff = {
-    id: number;
-    firstname: string;
-    lastname: string;
-    appointments: Appointment[];
-    userId: number;
-    user: User;
-    doctor_type: string | null;
-    price: number | null;
-    description: string | null;
-    specification: string | null;
-    work: string | null;
-  };
-  
-  export type MedicationAllocation = {
-    id: number;
-    patientId: number;
-    time_stamp: Date;
-    notified: boolean;
-    medicine: ViewMedicine[];
-    heart_rate: number;
-    hemoglobin: number;
-  };
+  id: number;
+  firstname?: string | null;
+  lastname?: string | null;
+  medicationAllocations: MedicationAllocation[];
+  appointments: Appointment[];
+  userId: number;
+  user: User;
+};
 
-  export type ViewMedicine = {
-    id: number;
-    medicine_name: string;
-    dosage: number;
-    med_allocationId: number
-    period: Period[]
-  };
-  
-  export type Medicine = {
-    id: number;
-    med_name: string;
-    dosage: number;
-  };
-  
-  export type Period = {
-    id: number;
-    period_name: string;
-    time: Date;
-  };
-  
-  export type Appointment = {
-    id: number;
-    patientId: number;
-    patient: Patient;
-    staffId: number;
-    staff: Staff;
-    appointmentTime: Date;
-  };
-  
-  export type QueryMedicine = {
-    id: number;
-    medicine_name: string;
-    dosage: number;
-    period: Period[]
-  };
+export type Staff = {
+  id: number;
+  firstname: string;
+  lastname: string;
+  userId: number;
+  doctor_type: string | null;
+  price: number | null;
+  description: string | null;
+  specification: string | null;
+  work: string | null;
+};
+
+export type MedicationAllocation = {
+  id: number;
+  patientId: number;
+  time_stamp: Date;
+  notified: boolean;
+  medicine: ViewMedicine[];
+  heart_rate: number;
+  hemoglobin: number;
+};
+
+export type ViewMedicine = {
+  id: number;
+  medicine_name: string;
+  dosage: number;
+  med_allocationId: number
+  period: Period[]
+};
+
+export type Medicine = {
+  id: number;
+  med_name: string;
+  dosage: number;
+};
+
+export type Period = {
+  id: number;
+  period_name: string;
+  time: Date;
+};
+
+
+export type QueryMedicine = {
+  id: number;
+  medicine_name: string;
+  dosage: number;
+  period: Period[]
+};
+
+export type Appointment = {
+  id: number;
+  patientId: number;
+  staffId: number;
+  appointmentTime: Date;
+};
